b1307c1cdad1939e283ffdb012927f1bc0bd89b0
Thomaz Soares <thomazthz0@gmail.com>
2018-10-17 20:35:44 -0300

    Add utils.py module and pprint_thread function

---
+++ idwall_tools/utils.py
@@ -0,0 +1,27 @@
+from operator import attrgetter
+
+from terminaltables import SingleTable
+
+
+def pprint_thread(thread):
+    extra_padding = 8
+    values = attrgetter(*thread._fields)(thread)
+
+    thread_data = []
+    l_columns = 0
+    for k, v in zip(thread._fields, values):
+        l_columns = max([len(k), l_columns])
+        thread_data.append((k.upper(), v))
+
+    l_columns += extra_padding
+
+    print()
+    for line in thread_data:
+        # print('-' * (l_columns + 1 + len(str(line[1]))))
+        print(f'{line[0]:{l_columns}} {line[1]}')
+        # print('-' * (l_columns + 1 + len(str(line[1]))))
+    print()
+
+
+def join_newline(it):
+    return '\n'.join(it)
