94d1c4bfe8b910014c8db328c00b7f7f221002ad
Thomaz Soares <thomazthz0@gmail.com>
2018-10-18 13:08:57 -0300

    Update textwrap cli options to allow the -f and --force flags

--- idwall_tools/cli.py
+++ idwall_tools/cli.py
@@ -12,11 +12,15 @@ def cli():
 
 
 @click.command(name='textwrap')
+@click.option('-f', '--force', is_flag=True)
 @click.option('-j', '--justified', is_flag=True)
 @click.option('-c', '--columns', default=40, type=int)
 @click.argument('text')
-def textwrap(text, columns, justified):
-    wrapped_text = wrap(text, columns=columns, justified=justified)
+def textwrap(text, columns, justified, force):
+    wrapped_text = wrap(text,
+                        columns=columns,
+                        justified=justified,
+                        force=force)
     for line in wrapped_text:
         click.echo(line)
 
